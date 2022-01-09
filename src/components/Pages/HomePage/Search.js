d9c9c234b2686b47f9b3ca4dddd3564d185b5b77
problemsolver <designangel2013@gmail.com>
2022-01-09 23:57:52 +0530

    Setup form submit for search

--- src/components/Pages/HomePage/Search.js
+++ src/components/Pages/HomePage/Search.js
@@ -1,39 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
 import { SearchIcon } from "@heroicons/react/outline";
+import { withRouter } from "react-router-dom";
 
-const Search = () => {
+const Search = (props) => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const searchServices = (e) => {
+    e.preventDefault();
+    if (searchTerm !== "") {
+      props.history.push(`/search/${searchTerm}`);
+    }
+  };
   return (
     <div className="bg-gray-50">
       <div className="max-w-7xl mx-auto py-14 px-4 sm:px-6 lg:py-20 lg:px-8 ">
-        <h2 className="text-3xl  text-gray-900 sm:text-4xl text-center">
-          <span className="block ">Looking for an expert?</span>
-          <span className="block text-indigo-600 ">
-            Find any expert of your choice.
-          </span>
-        </h2>
-        <div className="mt-8 flex lg:mt-0 lg:flex-shrink-0 w-full items-center justify-center pt-6">
-          <input
-            type="text"
-            className="border p-3 rounded-full w-1/2 pl-6"
-            placeholder="Doctors ..."
-          />
-          <button
-            type="submit"
-            className="bg-indigo-700 
-            text-white 
-            rounded-full 
-            p-3 pr-8 pl-8 -ml-32 hover:bg-indigo-900 hover:transition-all border-0"
-          >
-            <SearchIcon
-              className="h-4 w-4 inline-block -mt-1"
-              aria-hidden="true"
-            />{" "}
-            Search
-          </button>
+        <div class="grid grid-cols-2">
+          <div>
+            <h2 className="text-3xl  text-gray-900 sm:text-4xl">
+              <span className="block text-2xl font-bold">
+                Looking for an expert?
+              </span>
+              <span className="block text-3xl text-indigo-600 font-bold">
+                Find any expert of your choice.
+              </span>
+            </h2>
+
+            <form
+              className="mt-8 flex lg:mt-0 lg:flex-shrink-0 w-full items-center pt-6"
+              onSubmit={searchServices}
+            >
+              <input
+                type="text"
+                className="border p-3 rounded-full w-5/6 pl-6 border-indigo-500"
+                placeholder="Doctors ..."
+                value={searchTerm}
+                onChange={(e) => {
+                  setSearchTerm(e.target.value);
+                }}
+              />
+              <button
+                type="submit"
+                className="bg-indigo-700 
+                            text-white 
+                            rounded-full 
+                            p-3 pr-8 pl-8 -ml-32 hover:bg-indigo-900 hover:transition-all border-0"
+              >
+                <SearchIcon
+                  className="h-4 w-4 inline-block -mt-1"
+                  aria-hidden="true"
+                />{" "}
+                Search
+              </button>
+            </form>
+          </div>
+
+          <div>01</div>
         </div>
       </div>
     </div>
   );
 };
 
-export default Search;
+export default withRouter(Search);
